4b802b6f7c024e45bd27b525d15214c2aa4cb51b
vmuthyala <vmuthyala@GBWK-INA034036Z.galaxy.lan>
2020-01-22 15:59:03 +0530

    rendering list based on click of status button.

--- src/components/taskList.js
+++ src/components/taskList.js
@@ -4,20 +4,40 @@ class TaskList extends Component {
     constructor(props) {
 		super(props);
 		this.state = {
-            data : []
-        };
+            status : 'ALL',
+            taskList : [
+                { text : "First 1", completed : false},
+                { text : "Second 2", completed : false},
+                { text : "Third 3", completed : false},
+                { text : "Fourth 4", completed : false},
+                { text : "Fifth 5", completed : true}
+            ]
+        }
 	}
     render() {
+
+        let finalList;
+        switch(this.state.status) {
+            case 'ALL' :
+                finalList = this.state.taskList;
+                break
+            case 'PENDING' :
+                finalList = this.state.taskList.filter((item)=>!item.completed);
+                break
+            case 'COMPLETED' :
+                finalList = this.state.taskList.filter((item)=>item.completed);
+                break
+        }
         return(
             <div>
                 <div>
-                    <button type="submit" name="all">all</button>
-                    <button type="submit" name="pending">pending</button>
-                    <button type="submit" name="complete">complete</button>
+                    <button type="submit" name="all" onClick={()=>this.setState({status:'ALL'})}>all</button>
+                    <button type="submit" name="pending" onClick={()=>this.setState({status:'PENDING'})}>pending</button>
+                    <button type="submit" name="complete" onClick={()=>this.setState({status:'COMPLETED'})}>completed</button>
                 </div>
                  <ul>
                     {
-                        this.props.taskList.map(
+                        finalList.map(
                             (item, index)=> (
                                 <li key={index}>{item.text}</li>
                             )
